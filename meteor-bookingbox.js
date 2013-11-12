d55b4cf18b578c93e56fd7698c3aee392f359938
kmiekus <kmiekus@opentable.com>
2013-11-12 18:03:47 +0000

    datepicker and options generators

--- meteor-bookingbox.js
+++ meteor-bookingbox.js
@@ -9,12 +9,26 @@ if (Meteor.isClient) {
         });
         return Session.get('restaurant');
     };
+
+    Template.booking_box.hours = function () {
+        var hours = [];
+        for (var i = 0; i < 24; i++)
+        {
+            hours.push({text: i + ":00", val: i});
+            hours.push({text: i + ":30", val: i + 0.5});
+        }
+        return hours;
+    };
+
+    Template.booking_box.rendered = function () {
+        $('#datepicker').datepicker({weekStart:1});
+    };
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        var restaurant = Meteor.http.call("GET", "http://restaurants.services.opentable.com/v3/restaurants/68629");
-        Meteor._debug(restaurant.data);
-        Meteor._debug(Meteor.http.call("GET", "http://restaurants.services.opentable.com/v3/cuisines/en-GB/7003").data);
+//        var restaurant = Meteor.http.call("GET", "http://restaurants.services.opentable.com/v3/restaurants/68629");
+//        Meteor._debug(restaurant.data);
+//        Meteor._debug(Meteor.http.call("GET", "http://restaurants.services.opentable.com/v3/cuisines/en-GB/7003").data);
     });
 }
